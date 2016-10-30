6df6a5993a0d2a116abcc778a38b3f56ef305502
kgn95 <kaganozgun@gmail.com>
2016-10-30 22:29:17 +0200

    updated init_database.py
    updated init_database.py file on website because of console problems

--- init_database.py
+++ init_database.py
@@ -1,11 +1,12 @@
 import psycopg2 as dbapi2
 
 from dao.post import *
+from dao.user import *
 
 
 dsn = """user='vagrant' password='vagrant'
          host='localhost' port=5432 dbname='itucsdb'"""
-         
+
 
 def create_post_table():
     try:
@@ -25,15 +26,46 @@ def create_post_table():
         connection.rollback()
     finally:
         connection.close()
-    
-     
+
+
 def insert_post(post):
     try:
         with dbapi2.connect(dsn) as connection:
             cursor = connection.cursor()
             statement= """INSERT INTO POST(CONTENT,POSTDATE,USERID,SONGID) VALUES(%s,%s,%s,%s)"""
         cursor.execute(statement,(post.content,post.postdate,post.userid,post.songid))
-        connection.commit() 
+        connection.commit()
+        cursor.close()
+    except dbapi2.DatabaseError as e:
+        connection.rollback()
+    finally:
+        connection.close()
+
+def create_user_table():
+    try:
+        with dbapi2.connect(dsn) as connection:
+            cursor = connection.cursor()
+            statement = """CREATE TABLE IF NOT EXISTS USERDATA(
+            ID SERIAL PRIMARY KEY,
+            USERNAME  VARCHAR(50) UNIQUE NOT NULL,
+            PASSWORD VARCHAR(20) NOT NULL
+            )"""
+        cursor.execute(statement)
+        connection.commit()
+        cursor.close()
+    except dbapi2.DatabaseError:
+        connection.rollback()
+    finally:
+        connection.close()
+
+
+def insert_user(user):
+    try:
+        with dbapi2.connect(dsn) as connection:
+            cursor = connection.cursor()
+            statement= """INSERT INTO USERDATA(ID,USERNAME,PASSWORD) VALUES(%s,%s,%s)"""
+        cursor.execute(statement,(user.id,user.username,user.password))
+        connection.commit()
         cursor.close()
     except dbapi2.DatabaseError as e:
         connection.rollback()
